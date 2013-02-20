9e2b7858a3861ec0a187876954df9b3410a7d303
Sergi Hernando <sergi.hernando@mobivery.com>
2013-02-20 18:25:14 +0100

    Implementation of malcom.load-config initializer.

--- lib/malcom-rails3.rb
+++ lib/malcom-rails3.rb
@@ -1,5 +1,5 @@
-require 'malcom-rails3/version'
-require 'malcom-rails3/railtie' if defined?(Rails)
+require "malcom-rails3/version"
+require "malcom-rails3/railtie" if defined?(Rails)
 
 module Malcom
 end
--- lib/malcom-rails3/railtie.rb
+++ lib/malcom-rails3/railtie.rb
@@ -1,7 +1,20 @@
 module Malcom
+  extend self
+
   class Railtie < Rails::Railtie
-    initializer 'malcom' do
+    initializer "malcom.load-config" do
+      config_file = Rails.root.join("config", "malcom.yml")
+      raise "Malcom config file not present or not readable (#{config_file})." unless config_file && config_file.file?
+      Malcom.settings = YAML.load(ERB.new(File.new(config_file).read).result)[Rails.env]
     end
+  end
+
+  def settings
+    @settings ||= {}
+  end
+
+  def settings=(settings)
+    @settings = settings
   end
 end
 
